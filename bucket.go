6bdffa9615e0fb33389b213cad313ebf758548fe
ganmacs <ganmacs@gmail.com>
2017-03-01 23:53:01 +0900

    image

---
+++ bucket.go
@@ -0,0 +1,52 @@
+package curcuit
+
+import (
+	"sync"
+)
+
+type Bucket struct {
+	sucess      uint64
+	fail        uint64
+	totalSucess uint64
+	totalFail   uint64
+	request     uint64
+	mutex       sync.Mutex
+}
+
+func NewBucket() *Bucket {
+	return &Bucket{}
+}
+
+func (b *Bucket) ConsecutiveRate() float64 {
+	b.mutex.Lock()
+	defer b.mutex.Unlock()
+
+	return float64(b.fail) / float64(b.request)
+}
+
+func (b *Bucket) Sucess() {
+	b.mutex.Lock()
+	defer b.mutex.Unlock()
+
+	b.sucess++
+	b.totalSucess++
+	b.request++
+}
+
+func (b *Bucket) Fail() {
+	b.mutex.Lock()
+	defer b.mutex.Unlock()
+
+	b.fail++
+	b.totalFail++
+	b.request++
+}
+
+func (b *Bucket) Reset() {
+	b.mutex.Lock()
+	defer b.mutex.Unlock()
+
+	b.fail = 0
+	b.sucess = 0
+	b.request = 0
+}
---
+++ circuit.go
@@ -0,0 +1,94 @@
+package curcuit
+
+import (
+	"time"
+)
+
+const (
+	open     int = iota
+	closed   int = iota
+	halfopen int = iota
+)
+const (
+	DefaultTimeout = 60 * time.Second
+)
+
+type Configs struct {
+	rate float64
+}
+
+type CircuitBreaker struct {
+	state         int
+	step          uint64
+	bucket        Bucket
+	timeout       time.Duration
+	startOpenTime time.Time
+	rate          float64
+}
+
+type operation func() error
+
+func NewCircuitBreaker(config Configs) *CircuitBreaker {
+	return &CircuitBreaker{
+		state:         closed,
+		step:          0,
+		bucket:        *NewBucket(),
+		timeout:       DefaultTimeout,
+		startOpenTime: time.Now(),
+	}
+}
+
+func (cb *CircuitBreaker) isTimeout() bool {
+	return cb.timeout < time.Now().Sub(cb.startOpenTime)
+}
+
+func (cb *CircuitBreaker) HalfOpen() {
+	cb.state = halfopen
+}
+
+func (cb *CircuitBreaker) Sucess() {
+	if cb.state == halfopen {
+		cb.state = closed
+		cb.bucket.Reset()
+		cb.bucket.Sucess()
+	} else if cb.state == closed {
+		cb.bucket.Sucess()
+	}
+}
+
+func (cb *CircuitBreaker) exceedThreshold() bool {
+	return cb.bucket.ConsecutiveRate() > cb.rate
+}
+
+func (cb *CircuitBreaker) Fail() {
+	if cb.state == halfopen {
+		cb.state = open
+		cb.bucket.Fail()
+		cb.startOpenTime = time.Now()
+	} else if cb.state == closed {
+		cb.bucket.Fail()
+		if cb.exceedThreshold() {
+			cb.state = closed
+		}
+	}
+}
+
+func (cb *CircuitBreaker) Run(op operation) error {
+	if cb.state == open && cb.isTimeout() {
+		cb.HalfOpen()
+	}
+
+	if cb.state == open {
+		return nil // cb is open todo error type
+	}
+
+	err := op()
+
+	if err == nil {
+		cb.Sucess()
+	} else {
+		cb.Fail()
+	}
+
+	return err
+}
